7196a23d7a04ec9eaaa8bd02f4034b6c39531009
Cristian Gareca <cristiandanielgareca@gmail.com>
2026-03-04 14:44:46 -0300

    Correccion alias

--- scripts/01_Monthly_Sales_Architecture.sql
+++ scripts/01_Monthly_Sales_Architecture.sql
@@ -7,20 +7,18 @@
 -- Objetivo: Extraer el total de ventas mensual normalizado por fecha.
 -- Se utiliza un CTE para separar el cálculo del subtotal de la agrupación final.
 
-WITH Ventas_Parciales AS (
+WITH Ventas_Detalladas AS (
     SELECT
         strftime('%Y-%m', o.OrderDate) AS Mes,
-        -- Cálculo: Precio * Cantidad * (1 - Descuento)
-        od.UnitPrice * od.Quantity * (1 - od.Discount) AS Venta
+        od.UnitPrice * od.Quantity * (1 - od.Discount) AS Monto_Neto
     FROM [Order Details] AS od
     INNER JOIN Orders AS o ON o.OrderID = od.OrderID
 )
-
 -- Consulta final: Agrupa por mes y redondea el resultado a 2 decimales.
 
 SELECT
     Mes,
-    ROUND(SUM(Venta), 2) AS Venta_Total
-FROM Ventas_Parciales
+    ROUND(SUM(Monto_Neto), 2) AS Total_Facturado
+FROM Ventas_Detalladas
 GROUP BY Mes
 ORDER BY Mes ASC;
